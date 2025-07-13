3f61b4ec3ab079497b6f90de4c7341e1e101770d
Nikhil Kumar Singh <sing9999nikhil@gmail.com>
2025-07-14 01:23:55 +0530

    Downgrade tailwind css to "tailwindcss": "^3.4.1" and change confugrations, login.jsx also completed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+});
