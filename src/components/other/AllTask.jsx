37c680f475e02f561cb55f013b5ba0e650d1a64f
Nikhil Kumar Singh <sing9999nikhil@gmail.com>
2025-07-16 02:31:01 +0530

    Login logic

---
+++ src/components/other/AllTask.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto'>
+        <div className='bg-pink-500 mt-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Nikhil</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-green-500 mt-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Nikhil</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-teal-500 mt-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Nikhil</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-sky-500 mt-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Nikhil</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-purple-500 mt-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Nikhil</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-violet-500 mt-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Nikhil</h2>
+            <h3>Make a UI design</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
--- src/components/other/TaskListsNumbers.jsx
+++ src/components/other/TaskListsNumbers.jsx
@@ -9,19 +9,15 @@ const TaskListsNumbers = () => {
         </div>
         <div className='w-[45%] p-10 rounded-xl px-6 py-9 bg-blue-500'>
             <h2 className='text-2xl font-bold'>1</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h3 className='text-xl font-medium'>Completed Task</h3>
         </div>
         <div className='w-[45%] p-10 rounded-xl px-6 py-9 bg-yellow-500'>
             <h2 className='text-2xl font-bold'>2</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h3 className='text-xl font-medium'>Accepted Task</h3>
         </div>
         <div className='w-[45%] p-10 rounded-xl px-6 py-9 bg-pink-500'>
             <h2 className='text-2xl font-bold'>3</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
-        </div>
-        <div className='w-[45%] p-10 rounded-xl px-6 py-9 bg-emerald-400'>
-            <h2 className='text-2xl font-bold'>4</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h3 className='text-xl font-medium'>Failed Task</h3>
         </div>
     </div>
   )
